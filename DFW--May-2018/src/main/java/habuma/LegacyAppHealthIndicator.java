7cf4f555c1b8f6a37f020dd0ddbe834b9b01d0e1
Craig Walls <craig@habuma.com>
2018-05-20 08:16:36 -0500

    Disable custom health indicator so that admin looks green

--- DFW--May-2018/src/main/java/habuma/LegacyAppHealthIndicator.java
+++ DFW--May-2018/src/main/java/habuma/LegacyAppHealthIndicator.java
@@ -2,9 +2,8 @@ package habuma;
 
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class LegacyAppHealthIndicator 
 		implements HealthIndicator {
 
