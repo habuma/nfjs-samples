4ad9f9e75d4540a518dc5f53e1ccb57dc2c324f9
Craig Walls <craig@habuma.com>
2018-10-27 21:17:36 -0700

    Essential Boot, SEA Oct 2018

---
+++ SEA-Oct-2018/sea-books/src/main/java/habuma/HelloController.java
@@ -0,0 +1,16 @@
+package habuma;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		
+		
+		return "Hello Redmond!!!!!!!!";
+	}
+	
+}
---
+++ SEA-Oct-2018/sea-books/src/main/java/habuma/SeaBooksApplication.java
@@ -0,0 +1,13 @@
+package habuma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaBooksApplication.class, args);
+	}
+
+}
