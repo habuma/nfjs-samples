e89be370d8f8b6e27f7eaba622501fdb300d4328
Craig Walls <craig@habuma.com>
2018-11-11 08:01:01 -0500

    Rename main class to be less city-specific

--- IAD-Nov-2018/iad-profiles/src/main/java/habuma/ProfilesApplication.java
+++ IAD-Nov-2018/iad-profiles/src/main/java/habuma/ProfilesApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SeaProfilesApplication {
+public class ProfilesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SeaProfilesApplication.class, args);
+		SpringApplication.run(ProfilesApplication.class, args);
 	}
 	
 	@Bean
